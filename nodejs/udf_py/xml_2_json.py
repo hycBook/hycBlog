f46634fb14248492326149680d93033e3dfb933c
narutohyc <1832044043@qq.com>
2022-11-01 21:38:25 +0800

    更新

--- nodejs/udf_py/xml_2_json.py
+++ nodejs/udf_py/xml_2_json.py
@@ -41,17 +41,47 @@ def start(file_path):
     # 在集合中获取所有电影
     entrys = collection.getElementsByTagName("entry")
 
-    res = []
+    newDoc = xml.dom.minidom.Document()
+    rootNode = newDoc.createElement('search')
+    import html
+
+    from bs4 import BeautifulSoup
+
     for entry in entrys:
         title = get_test(entry, 'title')
         url = get_test(entry, 'url')
         content = get_test(entry, 'content')
-        res.append({'title': title, 'url': url, 'content': content})
+        new_entry = newDoc.createElement('entry')
 
-    json_dump_op('search.json', res)
+        new_title = newDoc.createElement('title')
+        phone_title_value = newDoc.createTextNode(title)
+        new_title.appendChild(phone_title_value)
+
+        new_url = newDoc.createElement('url')
+        phone_url_value = newDoc.createTextNode(url)
+        new_url.appendChild(phone_url_value)
+
+        new_content = newDoc.createElement('content')
+
+        res = BeautifulSoup(content, 'lxml')
+        phone_content_value = newDoc.createTextNode(res.text)
+        new_content.appendChild(phone_content_value)
+
+        new_entry.appendChild(new_title)
+        new_entry.appendChild(new_url)
+        new_entry.appendChild(new_content)
+        rootNode.appendChild(new_entry)
+
+    newDoc.appendChild(rootNode)
+
+    ori_str = html.unescape(newDoc.toxml())
+    with open("public/search.txt", "w", encoding="utf-8") as f:
+        newDoc.writexml(f, indent='', addindent='\t', newl='\n', encoding='utf-8')
+    print(ori_str)
     print("xml转json结束")
 
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
+    # file_path = sys.argv[1]
+    file_path = r"D:\桌面\search.xml"
     start(file_path=file_path)
